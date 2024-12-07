1e0d41314a9557121bd545ffdb8b4a534e19d629
Martin Pereira <martinpereirap@gmail.com>
2024-12-07 11:00:32 -0300

    Agregando pass database

--- config/db.php
+++ config/db.php
@@ -1,16 +1,23 @@
 <?php
-    class db{
-        private $host="localhost";
-        private $dbname="login";
-        private $user="root";
-        private $password="";
-        public function conexion(){
-            try {
-                $PDO = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
-                return $PDO;
-            } catch (PDOException $e) {
-                return $e->getMessage();
-            }
+class db
+{
+    private $host = "localhost";
+    private $dbname = "login";
+    private $user = "root";
+    private $password = "9884";
+
+    /**
+     * Funcion para conectar a la base de datos
+     * 
+     * @return PDO
+     */
+    public function conexion()
+    {
+        try {
+            $PDO = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->user, $this->password);
+            return $PDO;
+        } catch (PDOException $e) {
+            return $e->getMessage();
         }
     }
-?>
+}
