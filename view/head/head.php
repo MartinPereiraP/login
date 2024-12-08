68c422617c8602cedf2d041234ef1da94504298b
Martin Pereira <martinpereirap@gmail.com>
2024-12-08 12:01:31 -0300

    descomentar libreria js fontawesome

--- view/head/head.php
+++ view/head/head.php
@@ -8,7 +8,7 @@
   <title>Bootstrap demo</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   <link rel="stylesheet" href="/asset/css/estilos.css">
-  <!-- <script src="https://kit.fontawesome.com/e1d55cc160.js" crossorigin="anonymous"></script> -->
+  <script src="https://kit.fontawesome.com/e1d55cc160.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
