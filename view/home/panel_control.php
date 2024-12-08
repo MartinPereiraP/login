7b33e09b0bd2d25dfbad5d67364322e8fd129812
Martin Pereira <martinpereirap@gmail.com>
2024-12-08 15:26:42 -0300

    agragand estructuar HTML y retorno

--- view/home/panel_control.php
+++ view/home/panel_control.php
@@ -7,7 +7,16 @@ if (empty($_SESSION['usuario'])) {
     header("Location:login.php");
 }
 ?>
-<h1 class="text-center mt-4">Bienvenido <?= $_SESSION['usuario'] ?></h1>
+<section class="mt-5">
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center mt-4">Bienvenido <?= $_SESSION['usuario'] ?></h1>
+                <a href="/">Retornar</a>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 require_once base_path('view/head/footer.php');
 ?>
