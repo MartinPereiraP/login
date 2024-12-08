83a37384e82aa4a1d5879571cff3a22bc8f1bfdc
Martin Pereira <martinpereirap@gmail.com>
2024-12-08 14:53:55 -0300

    Mostrar errores en pantalla

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 // index.php
 
+// Mostrar errores en pantalla
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
 // Incluir la configuración global
 require_once __DIR__ . '/config/app.php';
 
